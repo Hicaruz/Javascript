935f15f29673099d4fb0501ed99bf112f70a326d
Juan Castillo <Juanpablodlcc@gmail.com>
2020-03-11 16:11:15 -0600

    best practices

--- estimate_pi.js
+++ estimate_pi.js
@@ -1,6 +1,6 @@
 const estimate_pi = n => {
     const nums = { circle: 0, total: 0 }
-    for (let i = 0; i < n; i++) {
+    for (;n > 0; n--) {
         const [x, y] = [Math.random(), Math.random()]
         const distance = x ** 2 + y ** 2
         distance <= 1 && nums.circle++
@@ -8,6 +8,6 @@ const estimate_pi = n => {
     }
     return 4 * nums.circle / nums.total
 }
-console.log(estimate_pi(100000000))
+console.log(estimate_pi(1000000))
 
 
