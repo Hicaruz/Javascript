8f3c339bebf37af392f008dbee37f31f33c044e2
Juan Castillo <Juanpablodlcc@gmail.com>
2020-03-22 14:20:53 -0600

    dom mar 22 2:20pm

--- estimate_pi.js
+++ estimate_pi.js
@@ -1,13 +1,11 @@
 const estimate_pi = n => {
-    const nums = { circle: 0, total: 0 }
-    for (;n > 0; n--) {
-        const [x, y] = [Math.random(), Math.random()]
-        const distance = x ** 2 + y ** 2
-        distance <= 1 && nums.circle++
-        nums.total++
-    }
-    return 4 * nums.circle / nums.total
-}
-console.log(estimate_pi(1000000))
-
-
+  const nums = { circle: 0, total: 0 };
+  for (; n > 0; n--) {
+    const [x, y] = [Math.random(), Math.random()];
+    const distance = x ** 2 + y ** 2;
+    distance <= 1 && nums.circle++;
+    nums.total++;
+  }
+  return (4 * nums.circle) / nums.total;
+};
+console.log(estimate_pi(100000000));
